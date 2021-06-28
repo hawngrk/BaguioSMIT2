b46b3ff7a6ae10d6b32c2840d4202b6ac8102c8c
Hudson Kit <47551487+hawngrk@users.noreply.github.com>
2021-06-28 20:32:33 +0800

    Session Handling added (to be edited)

---
+++ AdminBackEnd/sessionHandling.php
@@ -0,0 +1,13 @@
+<?php
+/***
+ * @Author Hudson Kit P. Natividad
+ * Date Created: June 28, 2021
+ * Description: allow session to start when credentials needed are enetered correctly.
+ */
+
+//initial session handling subject to change - Natividad Hudson Kit P.
+session_start();
+if(!empty($_SESSION['Email'])) {
+} else {
+    header("Location:../Admin/Login.html");
+}
