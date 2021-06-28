2d5d3483a1dcf454bac335fa02453dff2df4019a
Hudson Kit <47551487+hawngrk@users.noreply.github.com>
2021-06-28 20:33:01 +0800

    getReport.php added for ManageReport backend (to be edited)

---
+++ require/getReport.php
@@ -0,0 +1,23 @@
+<?php
+include("../includes/database.php");
+
+/***
+ * @Author Hudson Kit P. Natividad
+ * Date Created: June 28, 2021
+ * Description: returns the Report content from the Database
+ */
+
+//insert query
+//$query = '';
+//$stmt = $database->stmt_init();
+//$stmt -> prepare($query);
+//insert variables
+//$stmt -> bind_result();
+//$stmt -> execute();
+
+//$report = [];
+//while($stmt -> fetch()) {
+//    $report = new report(); // insert the varables needed
+//    $report->set_reportID($); // insert the varibles needed
+//    $report[] = $report;
+//}
