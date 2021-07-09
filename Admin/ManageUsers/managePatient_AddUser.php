d77f74a73eab50017b1e0c3b75534baeed82dcaf
2192011 <2192011@slu.edu.ph>
2021-07-09 22:43:00 +0800

    1. edited managePatient_AddUser.php (added saving inputs to database) 2. added managePersonnel_AddUser.php

--- Admin/ManageUsers/managePatient_AddUser.php
+++ Admin/ManageUsers/managePatient_AddUser.php
@@ -92,71 +92,62 @@
     <p> <b> Allergy: </b> </p>
     <p> *Check the following allergies if the person has a certain allergy</p>
     <div class="form-check">
-        <input class="skin" type="checkbox" value="Skin" id="skin">
+        <input class="skin" type="checkbox" name="allergy[]" id="skin">
         <label class="optionSkin" for="skin"> Skin </label>
 
-        <input class="insect" type="checkbox" value="Insect" id="insect">
+        <input class="insect" type="checkbox"  name="allergy[]" id="insect">
         <label class="optionInsect" for="insect"> Insect </label>
 
-        <input class="latex" type="checkbox" value="Latex" id="latex">
+        <input class="latex" type="checkbox" name="allergy[]" id="latex">
         <label class="optionLatex" for="latex"> Latex </label>
 
-        <input class="none" type="checkbox" value="None" id="none">
+        <input class="none" type="checkbox" name="allergy[]" id="none">
         <label class="optionNone" for="none"> None </label>
-    </div>
 
-    <div>
-        <input class="food" type="checkbox" value="Food" id="food">
+        <input class="food" type="checkbox" name="allergy[]" id="food">
         <label class="optionFood" for="food"> Food </label>
 
-        <input class="pollen" type="checkbox" value="Pollen" id="pollen">
+        <input class="pollen" type="checkbox" name="allergy[]" id="pollen">
         <label class="optionPollen" for="pollen"> Pollen </label>
 
-        <input class="mold" type="checkbox" value="Mold" id="mold">
+        <input class="mold" type="checkbox" name="allergy[]" id="mold">
         <label class="optionMold" for="mold"> Mold </label>
 
-    </div>
-    <div>
-        <input class="medication" type="checkbox" value="Medication" id="medication">
+        <input class="medication" type="checkbox" name="allergy[]" id="medication">
         <label class="optionMedication" for="medication"> Medication </label>
 
-        <input class="bite" type="checkbox" value="Bite" id="bite">
+        <input class="bite" type="checkbox" name="allergy[]" id="bite">
         <label class="optionBite" for="bite"> Bite </label>
 
-        <input class="pet" type="checkbox" value="Pet" id="pet">
+        <input class="pet" type="checkbox" name="allergy[]" id="pet">
         <label class="optionPet" for="pet"> Pet </label>
     </div>
 
     <p> <b> Comorbidities: </b> </p>
     <div>
-        <input class="hypertension" type="checkbox" value="Hypertension" id="hypertension">
+        <input class="hypertension" type="checkbox" name="comorbidity[]" id="hypertension">
         <label class="optionHypertension" for="hypertension"> Hypertension </label>
 
-        <input class="diabetes" type="checkbox" value="Diabetes" id="diabetes">
+        <input class="diabetes" type="checkbox" name="comorbidity[]" id="diabetes">
         <label class="optionDiabetes" for="diabetes"> Diabetes </label>
 
-        <input class="none" type="checkbox" value="None" id="none">
+        <input class="none" type="checkbox" name="comorbidity[]" id="none">
         <label class="optionNone" for="none"> None </label>
-    </div>
 
-    <div>
-        <input class="heart" type="checkbox" value="Heart Diseases" id="heart">
+        <input class="heart" type="checkbox" name="comorbidity[]" id="heart">
         <label class="optionHeartDisease" for="heart"> Heart Diseases </label>
 
-        <input class="asthma" type="checkbox" value="Asthma" id="asthma">
+        <input class="asthma" type="checkbox" name="comorbidity[]" id="asthma">
         <label class="optionAsthma" for="asthma"> Asthma </label>
 
-        <input class="others" type="checkbox" value="others" id="others">
-        <label for="others"> Others (Please indicate): </label>
-        <input type="text" class="others" id="others">
-    </div>
-
-    <div>
-        <input class="kidney" type="checkbox" value="Kidney Disease" id="kidney">
+        <input class="kidney" type="checkbox" name="comorbidity[]" id="kidney">
         <label class="optionKidneyDisease" for="kidney"> Kidney Diseases </label>
 
-        <input class="cancer" type="checkbox" value="Asthma" id="cancer">
+        <input class="cancer" type="checkbox" name="comorbidity[]" id="cancer">
         <label class="optionCancer" for="cancer"> Cancer </label>
+
+        <label for="others"> Others (Please indicate): </label>
+        <input type="text" class="others" name="others" id="others">
     </div>
 </form>
 <?php
@@ -168,8 +159,52 @@ if (isset($_POST['addPatient'])) {
     $middleName = $_POST['middleName'];
     $suffix = $_POST['suffix'];
     $category = $_POST['category'];
+
+    $streetName = $_POST['streetName'];
+    $cityTown = $_POST['cityTown'];
+    $state = $_POST['state'];
+    $birthday = $_POST['birthday'];
+    $gender = $_POST['gender'];
+    $contactNumber = $_POST['contactNumber'];
+    $occupation = $_POST['occupation'];
+
+    $fullName = $lastName." ".$firstName." ".$middleName." ";
+    $address = $streetName." ".$cityTown." ".$state." ";
+
+    $allergy = $_POST['allergy'];
+    $allergies = array();
+    foreach($allergy as $aller) {
+        if ($aller == 'on') {
+            array_push($allergies, 'True');
+        } else {
+            array_push($allergies, 'False');
+        }
+    }
+    $comorbidity = $_POST['comorbidity'];
+    $comorbidities = array();
+    foreach($comorbidity as $comor) {
+        if ($comor == 'on') {
+            array_push($allergies, 'True');
+        } else {
+            array_push($allergies, 'False');
+        }
+    }
+    $others = $_POST['others'];
+
+    $query1 = "INSERT INTO patient (patient_full_name)  VALUE ('$fullName');";
+
+    $getQuery = "SELECT patient_id FROM patient WHERE patient_full_name='$fullName'";
+    $dbase= $database->stmt_init();
+    $dbase ->prepare($getQuery);
+    $dbase ->execute();
+    $dbase ->bind_result($patientid);
+    $dbase ->fetch();
+    $dbase ->close();
+
+    $query2 = "INSERT INTO patient_details (patient_details_id, patient_first_name, patient_last_name, patient_middle_name, patient_suffix, patient_category, patient_address, patient_birthdate, patient_gender, patient_contact_number, patient_occupation) VALUE ('$patientid', '$firstName', '$lastName', '$middleName', '$suffix', '$category', '$address', '$birthday', '$gender', '$contactNumber', '$occupation');";
+
+
 }
 ?>
-
 </body>
 </html>
---
+++ Admin/ManageUsers/managePersonnel_AddUser.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add User-Personnel</title>
+</head>
+<body>
+<p> Basic Information </p>
+<form id='personnelForm' method="post" enctype="multipart/form-data">
+    <div class="basicInfoForms">
+        <label for="lastName"> Last Name </label>
+        <input type="text" class="formControl" id="lastName" name="lastName">
+    </div>
+    <div class="basicInfoForms">
+        <label for="firstName"> First Name </label>
+        <input type="text" class="formControl" id="firstName" name="firstName">
+    </div>
+    <div class="basicInfoForms">
+        <label for="middleName"> Middle Name </label>
+        <input type="text" class="formControl" id="middleName" name="middleName">
+    </div>
+    <div class="basicInfoForms">
+        <label for="suffix"> Suffix Name </label>
+        <select class="formControl" id="suffix" name="suffix">
+            <option> Sr </option>
+            <option> Jr </option>
+            <option> I </option>
+            <option> II </option>
+            <option> III </option>
+        </select>
+    </div>
+    <div class="basicInfoForms">
+        <label for="role"> Role </label>
+        <select class="formControl" id="role" name="role">
+            <option> City Hall Employee </option>
+            <option> Vaccination Personnel </option>
+        </select>
+    </div>
+    <div class="basicInfoForms">
+        <label for="contactNumber"> Contact Number </label>
+        <input type="text" class="formControl" id="contactNumber" name="contactNumber">
+    </div>
+    <!--<button type="submit" class="formControl"> Add </button>-->
+</form>
+<?php
+echo " <button type='submit' name='addPersonnel' form='personnelForm' >Add Patient</button>";
+if (isset($_POST['addPersonnel'])) {
+    include './includes/database.php';
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $middleName = $_POST['middleName'];
+    $suffix = $_POST['suffix'];
+    $role = $_POST['role'];
+    $contactNumber =$_POST['contactNumber'];
+
+    $query = "INSERT INTO employee (employee_fisrt_name, employee_last_name, employee_middle_name, employee_suffix, employee_role, employee_contact_number)  VALUE ('$lastName', '$firstName', '$middleName', '$suffix', '$role', '$contactNumber');";
+
+    $stmt = $database->stmt_init();
+    $stmt->prepare($query);
+    $stmt->execute();
+    $stmt->close();
+
+    $username = str_replace(' ', '', $firstName);
+}
+?>
+<br>
+<br>
+<p> Here are the patients credentials </p>
+<form>
+    <div class="credentials">
+        <label for="userName"> Username </label>
+        <input type="text" readonly class="formControl" id="userName" value="@usernameExample">
+    </div>
+    <div class="credentials">
+        <label for="password"> Password </label>
+        <input type="text" readonly class="formControl" id="password" value="passwordExample">
+    </div>
+    <button type="submit" class="formControl"> Add </button>
+</form>
+</body>
+</html>
