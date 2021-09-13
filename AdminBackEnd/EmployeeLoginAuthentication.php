7f4bc74123f322779748a0234a03a6da5c329d4f
Adriel Hans <2191057@slu.edu.ph>
2021-09-14 04:56:29 +0800

    Added login authentication

--- AdminBackEnd/EmployeeLoginAuthentication.php
+++ AdminBackEnd/EmployeeLoginAuthentication.php
@@ -1,37 +1,48 @@
 <?php
+//include("../Admin/login.html");
+include("../includes/configure.php");
+//$username = $_POST["username"];
+//$password = $_POST["password"];
+$username = 'CMBing';
+$password = 'WriteFromYourFullSizedAorticPump';
 
-include("../includes/constructor.php");
-include("../Admin/login.html");
-include("../includes/database.php");
 
-$data = "SELECT employee_username, employee_password, employee_account_type, employee_picture FROM employee_account";
+$accountData = "SELECT * FROM employee_account WHERE employee_username = ?";
 
-$accounts = [];
+try {
+    $stmtacc = $database->prepare($accountData);
+    $accountQuery = $stmtacc->execute([$username]);
+    $accountDetails = $stmtacc->fetch(PDO::FETCH_ASSOC);
+    $employeeID = $accountDetails['employee_id']; 
 
-$result = $database->query($data);
+    if($username == $accountDetails['employee_username'] && $password ==    $accountDetails['employee_password']) {
 
-while($row = $result->fetch_assoc()) {
-    $accounts[] = new employeeAccounts($row['employee_username'], $row['employee_password'], $row['employee_account_type'], $row['employee_picture']) ;
-}
+        $employeeData = "SELECT * FROM employee WHERE employee_id = ?";
+        $stmtemp = $database->prepare($employeeData);
+        $empQuery = $stmtemp->execute([$employeeID]);
+        $employeeDetails = $stmtemp->fetch(PDO::FETCH_ASSOC);
+        $brId = $employeeDetails['barangay_id'];
+        $barangayData = "SELECT * FROM barangay WHERE barangay_id = ?";
+        $stmtbar = $database->prepare($barangayData);
+        $brQuery = $stmtbar->execute([$brId]);
+        $brDetails = $stmtbar->fetch(PDO::FETCH_ASSOC);
+        $accountInformation = empToArray($accountDetails, $employeeDetails, 
+        $brDetails);
 
-if(isset($_POST['loginButton'])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    foreach ($accounts as $accs){
-        $accInfo = $accs->getEmpUsername();
-        $passInfo = $accs->getEmpPassword();
-
-        if ($accInfo == $username && $passInfo == $password) {
-            session_start();
-            header("Location: ../Admin/index.html");
-            $_SESSION['username'] = $username;
-        }
+        
+        $_SESSION['account'] = $accountInformation;
+        echo $accountInformation['barangay'];
+    } else {
+        throw new Exception(header('HTTP/1.0 400 Invalid username or password'));
     }
 
-    echo "<script>alert('Username or Password incorrect!')</script>";
-    echo "<script>location.href='EmployeeLoginAuthentication.php'</script>";
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
 
-?>
 
+function empToArray($accData, $empData, $brDetails) {
+    $fullName = $empData['employee_first_name']." ".$empData['employee_last_name'];  
+    $account = array('empId' => $empData['employee_id'], 'name' => $fullName, 'role' => $empData['employee_role'], 'barangay' => $brDetails['barangay_name'], 'barangay_id' => $brDetails['barangay_id']);
+    return $account;
+}
