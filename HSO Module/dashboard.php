27af6095ab9cc23efdfc51543b9297f42794c5cb
Hudson Kit Natividad <kit@Hudsons-MacBook-Pro.local>
2021-10-27 17:38:12 +0800

    dashboard for HSO added

---
+++ HSO Module/dashboard.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>HSO | Dashboard</title>
+
+    <!-- Our Custom CSS -->
+    <link rel="icon" href="../img/FaviSMIT+.png" type="image/jpg">
+    <link href="../css/style.css" rel="stylesheet">
+
+    <!-- Bootstrap-->
+    <script crossorigin="anonymous" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script crossorigin="anonymous" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script crossorigin="anonymous" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" rel="stylesheet">
+     <!--jQuery-->
+     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    
+    <!-- Font Awesome JS -->
+    <script src="https://kit.fontawesome.com/fcdb0fe9f3.js" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <!-- SWAL-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <div class="sidebar-brand-icon">
+                <img src="../img/logoo.png" style="width: 104%; margin-bottom:-19%; margin-top:-5%;" alt="Baguio Logo">
+                </div>
+            </div>
+
+            <ul class="list-unstyled components">
+            <hr>
+            <h4 id="headingNav1"> Health Service Office </h4>
+            <hr>
+            <h5 id="headingNav2">
+                <script src="../javascript/showDateAndTime.js"></script>
+            </h5>
+            <hr>
+
+            <li class="active">
+                <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+            </li>
+            <li>
+                <a href="ManageVaccineHome.php"><i class="fas fa-syringe"></i> Manage Vaccine</a>
+            </li>
+            <li>
+                <a href="ManagePatientHome.php"><i class="fas fa-user-circle"></i> Manage Patients</a>
+            </li>
+            <li>
+                <a href="ManageDeployment.php"><i class="fas fa-truck"></i> Manage Deployment</a>
+            </li>
+            <li>
+                <a href="ManageReportHome.php"><i class="fas fa-sticky-note"></i> Reports</a>
+            </li>
+            <li>
+                <a href="aboutAdmin.html"><i class="fas fa-question"></i> About</a>
+            </li>
+        </ul>
+
+
+            <ul class="list-unstyled CTAs">
+                <button type="button" class="btn btn-info signOutPersonnel">
+                    <span>Sign Out</span>
+                </button>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+            <div class="container container-fluid displayDash">
+            <div class="row">
+                <div class="col">
+                    <div class="col firstRowDash secondDash firstDash">
+                        <h4 class="dashHeads">Vaccination Jabs / <strong>Today</strong></h4>
+                    </div>
+                        <div class="col displayDashSecond">
+                            <div class="col secondRow firstRowDash">
+                            <h4 class="dashHeads">Vaccination Status / <strong>per District</strong></h4>
+                            </div>
+                            <div class="col secondRow firstRowDash">
+                            <h4 class="dashHeads"><strong>per Priority Group</strong></h4>
+
+                            </div>
+                        </div>    
+                </div>
+            <div class="col-md-auto">
+                <div class="col-md-auto secondCol firstRowDash">
+                    <h4 class="dashHeads">Vaccines <strong>Inventory</strong> </h4>
+                </div>
+                <div class="displayDash2 firstRowDash">
+                    <h4 class="dashHeads smallBoxDash">Total Vaccines Administered</h4>
+                    <br>
+                    <h4 class="dashHeads smallBoxDash">Fully Vaccinated</h4>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    </body>
+
+</html>
+
+<style>
+    .displayDash2{
+        height: 16.5em;
+    }
+    .smallBoxDash {
+        padding-top: 5%;
+        text-align: center;
+        font-size: 20px;
+    }
+    .secondRow {
+        height: 29.8em;
+    }
+    .secondCol{
+        height: 24em;
+    }
+    .firstDash {
+        height: 10em;
+        padding: 0;
+    }
+
+    .firstRowDash {
+        border-radius: 12px;
+        background-color: white;
+        margin: 0.1em;
+    }
+
+    .dashHeads {
+        margin-left: 1%;
+        margin-top: 0.5%;
+    }
+
+    .displayDash {
+        display: inline-block;
+    }
+
+    .displayDashSecond{
+        margin-top: 1%;
+        display: inline-flex;
+    }
+</style>
