c64d8a617d5f37325d48d889cbe20e0d952cbddc
AdrielHMG <75245309+AdrielHMG@users.noreply.github.com>
2021-11-18 16:00:00 +0800

    Centralized database patch

--- includes/configure.php
+++ includes/configure.php
@@ -1,6 +1,6 @@
 <?php
-$db_user = "root";
-$db_pass = "";
+$db_user = "smit.project";
+$db_pass = "objectNotFound";
 $db_name = "smit+";
 
 $database = new PDO('mysql:host=192.168.0.108;dbname='. $db_name .';charset=utf8', $db_user, $db_pass);
