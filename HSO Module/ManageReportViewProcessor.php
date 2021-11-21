1ecdbc6df25590f466f2d9169f854125c0eb43ae
Kilrone <2192011@slu.edu.ph>
2021-11-22 02:49:02 +0800

    1. added ReportTemplate.docx (template file for generating reports) 2. edited ManageReportViewProcessor.php (edited parameters on creating word document reports)

--- HSO Module/ManageReportViewProcessor.php
+++ HSO Module/ManageReportViewProcessor.php
@@ -344,13 +344,25 @@ if (isset($_POST['download'])) {
     mkdir('reports');
 
     foreach ($reports as $rep) {
-        $getReportsQuery = "SELECT report.report_id, report.report_details, report.report_status, patient.patient_full_name, CONCAT(patient_details.patient_house_address, ' ', barangay.barangay_name,' ',barangay.city,' ', barangay.province) AS full_address, patient_details.patient_contact_number FROM report JOIN patient ON report.patient_id = patient.patient_id JOIN patient_details ON report.patient_id = patient_details.patient_id JOIN barangay ON barangay.barangay_id = patient_details.barangay_id WHERE report.report_id = $rep";
+        $getReportsQuery = "SELECT report.report_id, report.report_details, report.report_status, patient.patient_full_name, CONCAT(patient_details.patient_house_address, ' ', barangay.barangay_name,' ',barangay.city,' ', barangay.province) AS full_address, patient.date_of_second_dosage, patient.date_of_first_dosage, patient.second_dose_vaccination, patient.first_dose_vaccination, patient_details.patient_contact_number, vaccine.vaccine_name FROM report JOIN patient ON report.patient_id = patient.patient_id JOIN patient_details ON report.patient_id = patient_details.patient_id JOIN barangay ON barangay.barangay_id = patient_details.barangay_id JOIN patient_drive ON patient.patient_id = patient_drive.patient_id JOIN vaccine_lot ON patient_drive.vaccine_lot_id = vaccine_lot.vaccine_lot_id JOIN vaccine ON vaccine_lot.vaccine_id = vaccine.vaccine_id WHERE report.report_id = $rep";
         $stmt->prepare($getReportsQuery);
         $stmt->execute();
-        $stmt->bind_result($reportId, $reportDetails, $reportStatus, $patientName, $patientAddress, $patientNum);
+        $stmt->bind_result($reportId, $reportDetails, $reportStatus, $patientName, $patientAddress, $dateSecond, $dateFirst, $dosageSecond, $dosageFirst, $patientNum, $vaccine);
         $stmt->fetch();
 
         //$phpWord = new PhpWord();
+        $dosage = '';
+        $date = '';
+        if ($dosageSecond == '1') {
+            $dosage = 'Fully Vaccinated';
+            $date = $dateSecond;
+        } else if ($dosageFirst == '1') {
+            $dosage = 'First Dose';
+            $date = $dateFirst;
+        } else {
+            $dosage = 'Not Yet Vaccinated';
+            $date = 'N/A';
+        }
 
         $template = new TemplateProcessor('ReportTemplate.docx');
         $template->setValue('name', $patientName);
@@ -359,6 +371,9 @@ if (isset($_POST['download'])) {
         $template->setValue('address', $patientAddress);
         $template->setValue('number', $patientNum);
         $template->setValue('status', $reportStatus);
+        $template->setValue('vaccine', $vaccine);
+        $template->setValue('date', $date);
+        $template->setValue('dosage', $dosage);
         /*
         $phpWord->addFontStyle('titleFont', array('bold' => true, 'italic' => false, 'size' => 20));
         $phpWord->addParagraphStyle('title', array('align' => 'center', 'spaceAfter' => 100));
